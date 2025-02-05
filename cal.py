4861cf3122e1b790e6fa3c1eba9e3dfad25c8d7e
Vinayak <vinayakchhabra545.vc@gmail.com>
2025-02-05 14:14:40 +0530

    added multiplication feature

--- cal.py
+++ cal.py
@@ -1,5 +1,6 @@
 
 import addition
+import multiplication
 num = input("""Select the operation :
       operation available : 
       1. Addition 
@@ -12,4 +13,6 @@ num2 = int(input("Enter number 2:"))
 
 if num== "1":
     print( addition.addition(num1 , num2))
+elif num == '4':
+    print(multiplication.mul(num1 , num2))
 
