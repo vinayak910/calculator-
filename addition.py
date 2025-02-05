3b7d2943fac82ee1538d60425a1c0dc387141971
Vinayak <vinayakchhabra545.vc@gmail.com>
2025-02-05 14:10:33 +0530

    added addition feature

---
+++ addition.py
@@ -0,0 +1,5 @@
+def addition(num1 , num2):
+    return num1 + num2
+
+if __name__ == "__main__":
+    print(addition(1 , 2))
---
+++ cal.py
@@ -0,0 +1,15 @@
+
+import addition
+num = input("""Select the operation :
+      operation available : 
+      1. Addition 
+      2. Substraction
+      3. Division
+      4. Multiplication """)
+
+num1 = int(input("Enter number 1:"))
+num2 = int(input("Enter number 2:"))
+
+if num== "1":
+    print( addition.addition(num1 , num2))
+
